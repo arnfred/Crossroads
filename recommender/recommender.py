5278d88dbcf8d165f03f9b5f5388a7c1a907a371
trouleau <william.trouleau@epfl.ch>
2014-06-06 22:31:58 +0200

    added/removed blank lines

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -189,28 +189,8 @@ class ArXivRecommender():
 		self.conn = sqlite3.connect(self.db_path)
 		self.cursor = self.conn.cursor()
 
+
 # ========================================================================================================
-
-class UnknownIDException(Exception):
-	"""
-	Exception raised if a unknown paper id is queried
-	"""
-	def __init__(self, paper_id):
-		self.paper_id = paper_id
-
-	def __str__(self):
-		return repr("UnknownIDException: Unknown paper id %s" % self.paper_id)
-
-class UnknownAuthorException(Exception):
-	"""
-	Exception raised if a unknown paper id is queried
-	"""
-	def __init__(self, author):
-		self.paper_id = author
-
-	def __str__(self):
-		return repr("UnknownAuthorException: Unknown author %s" % self.author)
-
 # ========================================================================================================
 
 class MiscellaneousData(tables.IsDescription):
